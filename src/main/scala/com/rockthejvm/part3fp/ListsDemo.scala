7248f9cf30b7ad6a065e2df985ffd4d04e55d6a5
Makarov Andrey <winandfx@gmail.com>
2024-04-25 08:54:27 +0300

    list exercises

--- src/main/scala/com/rockthejvm/part3fp/ListsDemo.scala
+++ src/main/scala/com/rockthejvm/part3fp/ListsDemo.scala
@@ -35,6 +35,41 @@ object ListsDemo {
   // "Higher-order functions" (e.g. map, filter) take other functions as arguments or return functions as a result
   // `flatMap` applies a function on every element, that returns new lists, then the lists are combined into one list
   val expandedList = aList.flatMap(x => List(x, x + 1))
+
+  /**
+   * Exercises
+   */
+  def scalaRepetitions(l: List[Int]): List[String] = {
+    def repeatScala(n: Int): String =
+      if (n <= 0) ""
+      else "scala" + repeatScala(n - 1)
+    l.map(repeatScala)
+  }
+  val exercise1 = scalaRepetitions(List(1, 2, 3, 0, 1))
+
+  def naturalNumbers(n: Int): List[Int] = {
+    if (n <= 0) throw IllegalArgumentException("Argument should be positive number")
+    else if (n == 1) List(1)
+    else naturalNumbers(n - 1) :+ n
+  }
+  val exercise2 = naturalNumbers(100)
+
+  def extractPrimes(l: List[Int]): List[Int] =
+    l.filter(x =>
+      if (x == 1) false
+      else !naturalNumbers(x / 2).tail.exists(p => (x % p) == 0)
+    )
+  val exercise3 = extractPrimes(exercise2)
+
+  def splitIntoDigits(l: List[Int]): List[Int] = {
+    def getDigits(v: Int): List[Int] = {
+      if (v == 0) List()
+      else getDigits(v / 10) :+ (v % 10)
+    }
+    l.flatMap(getDigits)
+  }
+  val exercise4 = splitIntoDigits(List(2, 10, 583, 46, 4))
+
   def main(args: Array[String]): Unit = {
     println(aList)
     println(biggerList)
@@ -47,5 +82,10 @@ object ListsDemo {
     println(scalaList)
     println(evenNumbers)
     println(expandedList)
+
+    println(exercise1)
+    println(exercise2)
+    println(exercise3)
+    println(exercise4)
   }
 }
